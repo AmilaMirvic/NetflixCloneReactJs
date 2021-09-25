d30b55d89600a2bceff33de15864b4e3ce1c116b
AmilaMirvic <amilamirvic.267@gmail.com>
2021-09-25 16:09:53 +0200

    Updates

---
+++ precache-manifest.54994c0826aca4140072fffa656ee675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934032cec84ad411de758534eaf32c7c",
+    "url": "/NetflixCloneReactJs/index.html"
+  },
+  {
+    "revision": "7b75c93135beb2d47631",
+    "url": "/NetflixCloneReactJs/static/css/main.020e2537.chunk.css"
+  },
+  {
+    "revision": "4b8ab644eadee92b0d42",
+    "url": "/NetflixCloneReactJs/static/js/2.363f80fd.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/NetflixCloneReactJs/static/js/2.363f80fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b75c93135beb2d47631",
+    "url": "/NetflixCloneReactJs/static/js/main.865f9cd4.chunk.js"
+  },
+  {
+    "revision": "5e87996ef3288113a37c",
+    "url": "/NetflixCloneReactJs/static/js/runtime-main.4afc9111.js"
+  }
+]);
